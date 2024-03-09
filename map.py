daec9a4986111ba93beb450ff239c41f988342f9
mamba <isaiahntina47@gmail.com>
2024-03-09 23:07:40 +0000

    new changes

---
+++ map.py
@@ -0,0 +1,48 @@
+from geopy.geocoders import Nominatim
+import pandas as pd
+import requests
+import folium
+
+import time
+
+def get_location_coordinates(address):
+   geolocator = Nominatim(user_agent="YourAppName (youremail@example.com)")
+   location = geolocator.geocode(address)
+   time.sleep(1)  # add a delay between requests
+   return location.latitude, location.longitude
+
+def get_directions_response(address1, address2, mode='drive'):
+   lat1, long1 = get_location_coordinates(address1)
+   lat2, long2 = get_location_coordinates(address2)
+   url = "https://route-and-directions.p.rapidapi.com/v1/routing"
+   key = "0a4691d35dmshb7a10d79387befdp140ee4jsnfd9a573b3f96"
+   host = "route-and-directions.p.rapidapi.com"
+   headers = {"X-RapidAPI-Key": key, "X-RapidAPI-Host": host}
+   querystring = {"waypoints":f"{str(lat1)},{str(long1)}|{str(lat2)},{str(long2)}","mode":mode}
+   response = requests.request("GET", url, headers=headers, params=querystring)
+   return response
+
+response = get_directions_response("19 Brown Street, Dundee", "10 Downing Street, London")
+
+def create_map(response):
+   # use the response
+   mls = response.json()['features'][0]['geometry']['coordinates']
+   points = [(i[1], i[0]) for i in mls[0]]
+   m = folium.Map()
+   # add marker for the start and ending points
+   for point in [points[0], points[-1]]:
+      folium.Marker(
+          location=point,
+          popup=folium.Popup('popup text here', max_width=250),
+          tooltip='tooltip text here'
+      ).add_to(m)
+   # add the lines
+   folium.PolyLine(points, weight=5, opacity=1).add_to(m)
+   # create optimal zoom
+   df = pd.DataFrame(mls[0]).rename(columns={0:'Lon', 1:'Lat'})[['Lat', 'Lon']]
+   sw = df[['Lat', 'Lon']].min().values.tolist()
+   ne = df[['Lat', 'Lon']].max().values.tolist()
+   m.fit_bounds([sw, ne])
+   return m
+m = create_map(response)
+m.save('static/maps/map.html')
