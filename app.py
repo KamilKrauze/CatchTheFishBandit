69925265f0973469de071b5734f1f4193c57b9f2
mamba <isaiahntina47@gmail.com>
2024-03-09 22:59:20 +0000

    new commit now resolved

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template, request
-import requests
-import json
-from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static')
 app.debug = True
@@ -9,6 +6,15 @@ app.debug = True
 @app.route('/')
 def home():
     return render_template('search.html')
+
+@app.route('/generate_map', methods=['GET', 'POST'])
+def map():
+    print('Request:', request.form)
+    current_location = request.args.get('current_location')
+    atm_id = request.args.get('atm_id') #lets assume this is the destination for now eg london
+    print('Current Location:', current_location)
+    print('ATM ID:', atm_id)
+    return render_template('custom_map.html')
 
 @app.route('/generate_map', methods=['GET', 'POST'])
 def map():
