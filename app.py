81874ad36c71759d453354450d6385249568c891
Kamil Krauze <72396466+KamilKrauze@users.noreply.github.com>
2024-03-09 21:47:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import requests
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,4 +9,4 @@ def home():
     return render_template('search.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
