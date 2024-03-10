81c4a6c861bd16b5f07c03e962b459967e49aa37
Rares Comanici <95580126+rarescomanici@users.noreply.github.com>
2024-03-10 03:04:48 +0000

    testing path

--- data_framing.py
+++ data_framing.py
@@ -7,14 +7,14 @@ app.debug = True
 
 # @app.route('/')
 # def index():
-    
+
 #     return 'Hello World!'
-    
+
 
 @app.route('/')
 def get_atms():
     # Define JSON file
-    with open("./HSBC_atms.json") as json_file:
+    with open("home/HSBC_atms.json") as json_file:
 
         # Convert JSON file to Python
         data = json.load(json_file)
@@ -54,27 +54,26 @@ def get_atms():
 
         print(jsonify(points))
 
-    # latitude = list(map(float, latitude))
-    # longitude = list(map(float, longitude))
+    latitude = list(map(float, latitude))
+    longitude = list(map(float, longitude))
 
-    # # removing extra fluff
-    # sep = ','
-    # for s in range(len(street)):
-    #     street[s] = street[s].split(sep, 1)[0]
+    # removing extra fluff
+    sep = ','
+    for s in range(len(street)):
+        street[s] = street[s].split(sep, 1)[0]
 
-    # for t in range(len(town)):
-    #     town[t] = town[t].split(sep, 1)[0]
-    #     town[t] = town[t].title()
+    for t in range(len(town)):
+        town[t] = town[t].split(sep, 1)[0]
+        town[t] = town[t].title()
 
-    # # making marker points list
-    # marker_points = []
-    # for i in range(len(latitude)):
-    #     marker_points.append({'Latitude': latitude[i], 'Longitude': longitude[i], 'Brand': brand[i], 'Street': street[i],
-    #                         'Town': town[i], 'Country': country[i], 'PostCode': postcode[i], 'SupportedLanguages': languages[i],
-    #                         'SupportedCurrency': currencies[i], 'ATMServices': services[i], 'ID': id[i]})
+    # making marker points list
+    marker_points = []
+    for i in range(len(latitude)):
+        marker_points.append({'Latitude': latitude[i], 'Longitude': longitude[i], 'Brand': brand[i], 'Street': street[i],
+                            'Town': town[i], 'Country': country[i], 'PostCode': postcode[i], 'SupportedLanguages': languages[i],
+                            'SupportedCurrency': currencies[i], 'ATMServices': services[i], 'ID': id[i]})
 
-    return jsonify(points)
-    # return jsonify({'data':marker_points})
+    return jsonify({'ATMs':marker_points})
 
 
 if __name__ == '__main__':
@@ -82,5 +81,5 @@ if __name__ == '__main__':
 
 # writing to csv
 #markers = pd.DataFrame(marker_points, columns=['Latitude', 'Longitude', 'Brand', 'Street', 'Town', 'Country', 'PostCode',
-                                               #'SupportedLanguages', "SupportedCurrency", "ATMServices", "ID"])
+#'SupportedLanguages', "SupportedCurrency", "ATMServices", "ID"])
 #markers.to_csv('atms.csv', index=False)
