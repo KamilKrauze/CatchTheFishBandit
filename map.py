dd53c78a04dcad6f0c6af872c995ec3f7b5e4e97
Rares Comanici <95580126+rarescomanici@users.noreply.github.com>
2024-03-10 04:00:47 +0000

    Revert "map_test.py"

--- map.py
+++ map.py
@@ -9,7 +9,6 @@ import json
 from flask import Flask
 from geopy.geocoders import Photon
 from folium import IFrame
-import os
 
 app = Flask(__name__)
 
@@ -88,8 +87,7 @@ def create_map(location):
     for index, atm in enumerate(visited_atms): 
         curr_atm = df_sites[df_sites['Identification'] == atm]
 
-        cd = os.path.abspath(os.getcwd())
-        with open(cd + '/popup.html', 'r') as f:
+        with open('./templates/popup.html', 'r') as f:
             popup_html = f.read()
         # create an IFrame using the HTML content
         iframe = IFrame(html=popup_html, width=500, height=300)
